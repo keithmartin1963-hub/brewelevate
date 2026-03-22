6b4a342780048b01bc1d7c3e5fb2ee812cc5dde3
keithmartin1963-hub <keithmartin1963@gmail.com>
2026-03-21 23:51:49 -0700

    Create page.tsx

---
+++ app/marketing/page.tsx
@@ -0,0 +1,8 @@
+export default function MarketingPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Build Test ✅</h1>
+      <p>If you see this, everything is working.</p>
+    </main>
+  );
+}
